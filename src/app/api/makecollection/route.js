25074af28cd4ab6ae2f06088e66d6da05256c5f9
leonardeganow <leonard@eganow.com>
2024-05-21 10:18:52 +0000

    updated credentials

--- src/app/api/makecollection/route.js
+++ src/app/api/makecollection/route.js
@@ -9,7 +9,7 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic"; // defaults to auto
 export async function POST(request) {
   const data = await request.json();
-  // console.log(data);
+  console.log(data);
   try {
     const response = await axios.post(
       `${BASE_URL}/transfer/debitaccount`,
--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -2,15 +2,15 @@ export const CREDIT_CARD_MONTH_REGEX = /^(0[1-9]|1[0-2])$/;
 export const CREDIT_CARD_YEAR_REGEX = /^\d{2}$/;
 export const CREDIT_CARD_CVV_REGEX = /^\d{3}$/;
 
-export const MERCHANT_USERNAME_ID ="GH0233009f674f824db5891d5717504236b3257b10d25948ddf63fd79c85a2e400e5d0";
+export const MERCHANT_USERNAME_ID =
+  "GH0233989a3ea3339fea2f4b8fe9b53ad5840756bf0576b8564487d2e5ac5769520a88xxxxDeom";
 
+export const MERCHANT_PASSWORD =
+  "90bed4a848ff28178aeae1279a6a150499cbab6e2d4d99ad33da2c16af6a87c49xxxxDeom";
 
-export const MERCHANT_PASSWORD ="a768032c213f134799e5d73278ad94f71aa5f8ab07046361550b3cce42c005ff";
+export const MERCHANT_AUTH =
+  "GH0233eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRvY2FtZW55b0BnbWFpbC5jb20iLCJjZXJ0c2VyaWFsbnVtYmVyIjoiMDIzMzIwMjIyOTIwMDAwMDAwMDEiLCJwcmltYXJ5c2lkIjoiRkxVVFdBR0gwMjMzU0MyMDEwMDAwMSIsIm5iZiI6MTY2NjI2MzgwOCwiZXhwIjoxNjY2MjY0MTA4LCJpYXQiOjE2NjYyNjM4MDh9.iQKFVfUvG3xJPuCoTTx25r43fgCd34";
 
-
-export const MERCHANT_AUTH = "GH0233eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJjZXJ0c2VyaWFsbnVtYmVyIjoiS09SQVBBR0gwMjMzU0MxMDEwMDAwMzAxIiwibmJmIjoxNzA3NDg5NDAwLCJleHAiOjE3MDc0ODk3MDAsImlhdCI6MTcwNzQ4OTQwMH0.N2408HxWA4huze259k3OgcKuKHNpFgn7ZqttkkZfGElpBP5EfpjmHnwuyWBghBr6xDSOu9juqZiSNNB04fonAQ";
-
-
-export const BASE_URL= "https://eganow-merchant.egadevapi.com/eganow/api"
+export const BASE_URL = "https://eganow-merchant.egadevapi.com/eganow/api";
 // export const BASE_URL= "https://wwh0vng7-5001.inc1.devtunnels.ms/eganow/api"
 export const Amount = 2;
