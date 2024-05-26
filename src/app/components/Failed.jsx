8764ff08e0aa3a147cf39b234fdce6e9a741115e
leonardeganow <leonard@eganow.com>
2024-05-26 21:40:29 +0000

    added failed and success queries to return url

--- src/app/components/Failed.jsx
+++ src/app/components/Failed.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
-import success from '../../../public/sucess.gif'
-import failed from '../../../public/remove.png'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import success from "../../../public/sucess.gif";
+import failed from "../../../public/remove.png";
+import Link from "next/link";
+import Image from "next/image";
 
 function Failed() {
-    return (
-        <div className='flex flex-col items-center mt-5 py-5'>
-            <div className='w-24 h-24 text-center flex justify-center items-center'>
-                <Image src={failed} alt="" width={300} height={300} />
-            </div>
-            <p className='text-xl font-semibold py-2 text-red-700'>Payment Failed !</p>
-            <small className='text-center text-gray-400 mb-5'>Unfortunately, your payment could not be processed. Please try again or use a different payment method.</small>
-           <div>
-            <Link href={'/'} className='bg-blue-500 my-6 px-4 py-2 text-white shadow'>Retry</Link>
-           </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center mt-5 py-5">
+      <div className="w-24 h-24 text-center flex justify-center items-center">
+        <Image src={failed} alt="" width={300} height={300} />
+      </div>
+      <p className="text-xl font-semibold py-2 text-red-700">
+        Payment Failed !
+      </p>
+      <small className="text-center text-gray-400 mb-5">
+        Unfortunately, your payment could not be processed. Please try again or
+        use a different payment method.
+      </small>
+      <div>
+        <Link
+          href={{
+            pathname: localStorage.getItem("callBack_url"),
+            query: { status: "failed" },
+          }}
+          className="bg-blue-500 my-4 md:px-4 md:py-2 p-2 text-sm md:text-base text-white shadow rounded "
+        >
+          Return to Merchant
+        </Link>{" "}
+      </div>
+    </div>
+  );
 }
 
-export default Failed
+export default Failed;
--- src/app/components/Success.jsx
+++ src/app/components/Success.jsx
@@ -1,44 +1,52 @@
-import React, { useEffect } from 'react'
-import success from '../../../public/sucess.gif'
-import animation from '../../../public/Animation.json'
-import Lottie from 'react-lottie';
-import Link from 'next/link'
-import Image from 'next/image'
+import React, { useEffect } from "react";
+import success from "../../../public/sucess.gif";
+import animation from "../../../public/Animation.json";
+import Lottie from "react-lottie";
+import Link from "next/link";
+import Image from "next/image";
 
 function Success() {
-    const defaultOptions = {
-        loop: false,
-        autoplay: true, 
-        animationData: animation,
-        rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
-        }
-      };
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
+  useEffect(() => {
+    localStorage.removeItem("amount");
+    localStorage.removeItem("token");
+    localStorage.removeItem("xauth");
+    localStorage.removeItem("transactionId");
+  }, []);
 
-      useEffect(()=>{
-        localStorage.removeItem("amount");
-        localStorage.removeItem("token");
-        localStorage.removeItem("xauth");
-        localStorage.removeItem("transactionId");
-      },[])
-     
-      
-
-    return (
-        <div className='flex flex-col items-center mt-5'>
-            <div className='w-auto h-24 text-center flex justify-center items-center'>
-            <Lottie options={defaultOptions}
-              height={200}
-              width={200}/>
-            </div>
-            <p className='text-xl font-semibold py-2 text-green-500'>Payment Successful !</p>
-            <small className='text-center text-gray-400 p-3 mb-4'>Thank you for your purchase. Your order has been placed successfully and is being processed.</small>
-           <div className='mb-3'>
-            <Link href={localStorage.getItem("callBack_url")} className='bg-blue-500 my-4 md:px-4 md:py-2 p-2 text-sm md:text-base text-white shadow rounded '>Return to Merchant</Link>
-           </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center mt-5">
+      <div className="w-auto h-24 text-center flex justify-center items-center">
+        <Lottie options={defaultOptions} height={200} width={200} />
+      </div>
+      <p className="text-xl font-semibold py-2 text-green-500">
+        Payment Successful !
+      </p>
+      <small className="text-center text-gray-400 p-3 mb-4">
+        Thank you for your purchase. Your order has been placed successfully and
+        is being processed.
+      </small>
+      <div className="mb-3">
+        <Link
+          href={{
+            pathname: localStorage.getItem("callBack_url"),
+            query: { status: "success" },
+          }}
+          className="bg-blue-500 my-4 md:px-4 md:py-2 p-2 text-sm md:text-base text-white shadow rounded "
+        >
+          Return to Merchant
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Success
+export default Success;
